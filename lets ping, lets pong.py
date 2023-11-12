dc541ec3e90805da0cbf170d7483c1678efca403
r5ne <renetimantsev@gmail.com>
2023-11-12 16:25:42 +0000

    added annotations allowed passing keys for 2 player paddles

--- lets ping, lets pong.py
+++ lets ping, lets pong.py
@@ -5,66 +5,65 @@ from tkinter import *
 
 # class to make multiple instances of the paddle objects
 class Paddle:
-    def __init__(self, can, start_x, start_y, size_x, size_y, color, tag):
+    def __init__(self, can, start_x, start_y, size_x, size_y, color, tag, keyup, keydown):
         self.can, self.color, self.tag = can, color, tag
         self.start_x, self.start_y, self.size_x, self.size_y = start_x, start_y, size_x, size_y
-        # paddle created
+        self.keyup, self.keydown = keyup, keydown
         self.paddle = self.can.create_rectangle((start_x, start_y, start_x + size_x, start_y + size_y), fill=color,
-                                                tag=self.tag)
-        self.yspeed = 0
-        self.can.bind_all('<KeyPress-Up>', self.move_up)
-        self.can.bind_all('<KeyPress-Down>', self.move_down)
+                                                tag=self.tag)  # paddle created
+        self.yspeed = 0  # paddle only moves in y direction
+        self.can.bind_all(self.keyup, self.move_up)  # bind specified keys to movement
+        self.can.bind_all(self.keydown, self.move_down)
 
-    def move(self):
+    def move(self):  # paddle boundary check
         self.can.move(self.paddle, 0, self.yspeed)
         pos = self.can.coords(self.paddle)
-        if pos[1] <= 0:
+        if pos[1] <= 0:  # y
             self.yspeed = 0
-        if pos[3] >= 700:
+        if pos[3] >= 700:  # y
             self.yspeed = 0
 
-    def move_up(self, evt):
+    def move_up(self, event):  # movements and paddle speed
         self.yspeed = -2
 
-    def move_down(self, evt):
+    def move_down(self, event):
         self.yspeed = 2
+
 
 # class to make multiple instances of the ball objects
 class Ball:
     def __init__(self, can, start_x, start_y, size_x, size_y, color, tag, pad):
         self.can, self.color, self.tag, self.pad = can, color, tag, pad
         self.start_x, self.start_y, self.size_x, self.size_y = start_x, start_y, size_x, size_y
-        # ball created
         self.ball = self.can.create_oval((start_x, start_y, start_x + size_x, start_y + size_y), fill=color,
-                                         tag=self.tag)
-        self.xspeed = 3
-        self.yspeed = random.randrange(-3,3)
+                                         tag=self.tag)  # ball created
+        self.xspeed, self.yspeed = 3, random.randrange(-3, 3)  # initial ball speed in x is randomised
 
-    def move(self):
+    def move(self):  # ball paddle/wall bouncing method
         self.can.move(self.tag, self.xspeed, self.yspeed)
-        pos = self.can.coords(self.tag)
+        pos = self.can.coords(self.tag)  # position of paddle
         print(pos)
-        if pos[1] <= 0:
-            self.yspeed *=-1
-        if pos[3] >= 700:
-            self.yspeed *=-1
-        if pos[0] <= 0:
-            self.xspeed *=-1
-        if pos[2] >= 1200:
-            self.xspeed *=-1
-        if self.hit_paddle(pos) == True:
-            self.yspeed = random.randrange(-3,3)
-            self.xspeed *=-1
+        if pos[1] <= 0:  # top wall
+            self.yspeed *= -1
+        if pos[3] >= 700:  # bottom wall
+            self.yspeed *= -1
+        if pos[0] <= 0:  # left wall
+            self.xspeed *= 1
+        if pos[2] >= 1200:  # right wall
+            self.xspeed *= -1
+        if self.hit_paddle(pos):
+            self.yspeed = random.randrange(-3, 3)  # randomise bounce of paddle
+            self.xspeed *= -1
 
-    def hit_paddle(self, pos):
+    def hit_paddle(self, pos):  # method to detect ball contact with paddle
         paddle_pos = self.can.coords(self.pad.tag)
         if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
-            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+            if paddle_pos[1] <= pos[3] <= paddle_pos[3]:
                 return True
         return False
 
 
-# paddle movement
+# old mouse paddle movement
 '''def callback(event, pad):
     constant_width = getattr(pad, 'start_x')
     w1 = getattr(pad, 'size_x') + getattr(pad, 'start_x')
@@ -73,8 +72,8 @@ class Ball:
     canvas.coords('pad', constant_width, y_event - move_height / 2, w1, y_event + move_height / 2)'''
 
 root = Tk()  # main window
-ws = root.winfo_screenwidth()  # width of root
-hs = root.winfo_screenheight()  # height of root
+ws = root.winfo_screenwidth()  # screen width
+hs = root.winfo_screenheight()  # screen height
 
 # calculate x and y coordinates for the Tk root window
 w = 1200  # width for the Tk root
@@ -91,11 +90,12 @@ canvas = Canvas(root, width=w, height=h)
 canvas.pack()
 
 # making paddles
-paddle1 = Paddle(canvas, 10, 10, 10, 100, 'blue', 'pad')
+paddle1 = Paddle(canvas, 10, 10, 10, 100, 'blue', 'pad', '<KeyPress-Up>', '<KeyPress-Down>')
 
 # making ball
 ball1 = Ball(canvas, 20, 20, 20, 20, 'yellow', 'ball', paddle1)
 
+# mainloop
 while True:
     ball1.move()
     paddle1.move()
